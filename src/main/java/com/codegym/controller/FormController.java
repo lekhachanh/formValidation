ea53339e4505c444150a96ec40520ffdff2102b1
lekhachanh <lekhachanh123@gmail.com>
2019-08-07 20:50:51 +0700

    git form validation

---
+++ src/main/java/com/codegym/controller/FormController.java
@@ -0,0 +1,34 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/view")
+    public String showForm(Model model){
+        model.addAttribute("user", new User());
+        return "/form/index";
+    }
+
+    @PostMapping("/check")
+    public String checkValidation(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+        new User().validate(user, bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "/form/index";
+        }else {
+            model.addAttribute("firstName", user.getFirstName());
+            model.addAttribute("lastName", user.getLastName());
+            model.addAttribute("phoneNumber", user.getPhoneNumber());
+            model.addAttribute("age", user.getAge());
+            model.addAttribute("email", user.getEmail());
+            return "/form/result";
+        }
+    }
+}
